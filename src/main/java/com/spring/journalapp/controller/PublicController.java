4ca68f4b67b65a6bc94c7b47ff5d7212674e8169
Varun <varunarya119@gmail.com>
2025-01-14 18:50:44 +0530

    User Endpoints Complete

---
+++ src/main/java/com/spring/journalapp/controller/PublicController.java
@@ -0,0 +1,29 @@
+package com.spring.journalapp.controller;
+
+import com.spring.journalapp.entity.User;
+import com.spring.journalapp.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/public")
+public class PublicController {
+    private UserService userService;
+
+    // Constructor Based DI:
+    public PublicController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
+
+    @PostMapping("/create-user")
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        userService.saveEntry(user);
+        return new ResponseEntity<>("User CREATED Successfully.", HttpStatus.CREATED);
+    }
+}
--- src/main/java/com/spring/journalapp/controller/UserController.java
+++ src/main/java/com/spring/journalapp/controller/UserController.java
@@ -1,49 +1,45 @@
 package com.spring.journalapp.controller;
 
 import com.spring.journalapp.entity.User;
+import com.spring.journalapp.repository.UserRepository;
 import com.spring.journalapp.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
     private UserService userService;
+    private UserRepository userRepository;
 
-    public UserController(UserService userService) {
+    // Constructor Based DI:
+    public UserController(UserService userService, UserRepository userRepository) {
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
-    @GetMapping
-    public ResponseEntity<?> getAll() {
-        List<User> list = userService.getEntries();
-        if (list != null && !list.isEmpty()) return new ResponseEntity<>(list, HttpStatus.OK);
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
+    @PutMapping
+    public ResponseEntity<?> updateUser(@RequestBody User user) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
 
-    @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        try {
-            userService.saveEntry(user);
-            return new ResponseEntity<>(user, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @PutMapping("{username}")
-    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable String username) {
         User userInDB = userService.findByUsername(username);
 
-        if (userInDB != null) {
-            userInDB.setUsername(user.getUsername());
-            userInDB.setPassword(user.getPassword());
-            userService.saveEntry(userInDB);
-            return new ResponseEntity<>(userInDB, HttpStatus.ACCEPTED);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        userInDB.setUsername(user.getUsername());
+        userInDB.setPassword(user.getPassword());
+        userService.saveEntry(userInDB);
+        return new ResponseEntity<>("User UPDATED Successfully.", HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+
+        userRepository.deleteByUsername(username);
+        return new ResponseEntity<>("User DELETED Successfully.", HttpStatus.NO_CONTENT);
     }
 }
